693ee42d32e45518833d8d4dbca29639919d83e9
Daniel Riazanovskiy <riazanovskiy@gmail.com>
2015-05-28 12:42:08 +0300

    WIP; mark working, reduce broken

--- cimpl.c
+++ cimpl.c
@@ -2,21 +2,85 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <time.h>
+
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 
 #define WORDSIZE 256
 
 char processor[WORDSIZE];
 
 char tables[100][WORDSIZE];
-char output[WORDSIZE];
 
 enum CLICOMMAND
 {
-    MAP, REDUCE, EXIT, OTHER
+    MAP, REDUCE, EXIT, MARK, OTHER
 };
+
+
+void splitFile(char* input, size_t nClusters)
+{
+    strncat(input, ".tbl", sizeof(input) - 1);
+
+    FILE* inputFile = fopen(input, "r");
+    fseek(inputFile, 0, SEEK_END);
+    size_t fileLength = (size_t) ftell(inputFile);
+
+    char* fileAddr = (char*) mmap(0, fileLength, PROT_READ, MAP_SHARED,
+                                  fileno(inputFile), 0);
+
+    size_t labels[nClusters + 1];
+    memset(labels, 0, (nClusters + 1) * sizeof(labels[0]));
+
+    size_t currCluster = 0;
+    for (size_t k = 0, i = 0; k < nClusters - 1; k++)
+    {
+        for (size_t j = 0; i < fileLength && j < (fileLength / nClusters + 1); i++, j++)
+        {
+            if (fileAddr[i] == '\n')
+            {
+                labels[currCluster++] = i;
+                i += 2 + fileLength / nClusters - j; // есть ли тут ошибка на один? Да наверняка
+                break;
+            }
+        }
+    }
+
+    labels[currCluster++] = fileLength;
+    nClusters = currCluster;
+
+    munmap(fileAddr, fileLength);
+    fclose(inputFile);
+
+    for (int i = 0, prevOffset = -1; i < nClusters; prevOffset = labels[i], i++)
+    {
+        char cmd[1000] = "";
+        snprintf(cmd, sizeof(cmd) - 1, "dd bs=1 if=%s skip=%i count=%zu of=marked_%s_%i",
+                 input, prevOffset + 1, labels[i] - prevOffset, input, i);
+        system(cmd);
+    }
+}
+
+int parseCommand(const char* command)
+{
+    int currentCommand = OTHER;
+    if (strcmp(command, "map") == 0)
+        currentCommand = MAP;
+    else if (strcmp(command, "reduce") == 0)
+        currentCommand = REDUCE;
+    else if (strcmp(command, "exit") == 0)
+        currentCommand = EXIT;
+    else if (strcmp(command, "mark") == 0)
+        currentCommand = MARK;
+    return currentCommand;
+}
 
 int main(int argc, char** argv)
 {
+    srand((uint)time(0));
+    system("ls");
     size_t nProcesses = 4;
 
     if (argc > 1)
@@ -26,12 +90,16 @@ int main(int argc, char** argv)
         nProcesses = (size_t) nProcessesArgument;
     }
 
+    size_t nClusters = nProcesses;
+
     FILE** processorHandles = (FILE**) malloc(sizeof(FILE*) * nProcesses);
     char *line = 0;
     size_t lineLength = 0;
 
     char command[WORDSIZE] = "";
     char cmd[1000] = "";
+
+    pid_t childrenPids[1000] = {};
 
     while (printf("1-day-till-deadline> "), getline(&line, &lineLength, stdin) >= 0)
     {
@@ -41,45 +109,37 @@ int main(int argc, char** argv)
         else
             continue;
 
-        int currentCommand = OTHER;
-        if (strcmp(command, "map") == 0)
-            currentCommand = MAP;
-        else if (strcmp(command, "reduce"))
-            currentCommand = REDUCE;
-        else if (strcmp(command, "exit"))
-            currentCommand = EXIT;
+        int currentCommand = parseCommand(command);
 
         if (MAP == currentCommand || REDUCE == currentCommand)
         {
-            if (nextToken = strtok(NULL, " \t\r\n"))
+            if ((nextToken = strtok(NULL, " \t\r\n")))
             {
                 strcpy(processor, nextToken);
             }
             else
             {
-                printf("No processor found!\n");
+                fprintf(stderr, "No processor found!\n");
                 continue;
             }
 
             int nTables = 0;
-            while (nextToken = strtok(NULL, " \t\r\n"))
-            {
-                strcpy(tables[nTables], nextToken);
-                strncat(tables[nTables], ".tbl", WORDSIZE);
-                nTables++;
-            }
+            while ((nextToken = strtok(NULL, " \t\r\n")))
+                strcpy(tables[nTables++], nextToken);
 
             if (!nTables)
             {
-                printf("No output file specified\n");
+                fprintf(stderr, "No output file specified\n");
                 continue;
             }
 
             --nTables;
+            char output[WORDSIZE];
             strcpy(output, tables[nTables]);
 
             if (REDUCE == currentCommand)
             {
+                assert(REDUCE != currentCommand);
                 for (int i = 0; i < nTables; ++i)
                 {
                     snprintf(cmd, sizeof(cmd) - 1, "sort --parallel=%lu %s > reduce_%i.tbl",
@@ -143,6 +203,21 @@ int main(int argc, char** argv)
         else if (EXIT == currentCommand)
         {
             break;
+        }
+        else if (MARK == currentCommand)
+        {
+            char input[1000] = "";
+            if ((nextToken = strtok(NULL, " \t\r\n")))
+            {
+                strcpy(input, nextToken);
+            }
+            else
+            {
+                fprintf(stderr, "No filename given\n");
+                continue;
+            }
+
+            splitFile(input, nClusters);
         }
         else
         {
