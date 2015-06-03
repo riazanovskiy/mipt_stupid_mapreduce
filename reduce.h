b6d9a6a32c92406cb43257eac1aaee3faea4b1c1
asverdlov <sverdlovalbert@gmail.com>
2015-06-03 14:16:25 +0300

    Added header guard just in case

--- reduce.h
+++ reduce.h
@@ -1,3 +1,8 @@
+#ifndef _REDUCE_H_
+#define _REDUCE_H_
+
 size_t runReducers(char **tableNames, size_t nTables, size_t nProcesses,
                    pid_t* pidPool, size_t pidUsed, const char* processor,
                    int operationId);
+
+#endif
